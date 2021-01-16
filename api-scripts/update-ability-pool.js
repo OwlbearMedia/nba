f0036d34c3ac98972e96341fb71462d50c90d5cd
Dylan Whitney <thedylanwhitney@gmail.com>
2021-01-16 12:53:38 -0600

    Adding work thus far to repo

---
+++ api-scripts/update-ability-pool.js
@@ -0,0 +1,17 @@
+// Function to adjust ability pool after spending points on a roll
+on('ready', () => {
+  on("chat:message", (msg) => {
+    if (msg.inlinerolls) {
+      // parsing roll result
+      const pointSpend = parseInt(msg.content.match(/{points=.+?(?=})/)[0].split('=')[1], 10);
+      const ability = msg.content.match(/{ability=.+?(?=})/)[0].split('=')[1].replace('-a', '').replace('^{', '');
+      const name = msg.content.match(/{name=.+?(?=})/)[0].split('=')[1];
+      // getting character info
+      const character = findObjs({ type: 'character', name })[0];
+      const characterAbility = findObjs({ type: 'attribute', characterid: character.id, name: `${ability}_pool` })[0];
+      const characterAbilityValue = parseInt(characterAbility.get('current'), 10);
+
+      characterAbility.set('current',  characterAbilityValue - pointSpend);
+    }
+  });
+});
---
+++ scripts.js
@@ -0,0 +1,105 @@
+const abilityValues = ['-11', '-10', '-9', '-8', '-7', '-6', '-5', '-4', '-3', '-2', '-1', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
+const abilities = ['athletics', 'conceal', 'cover', 'digital_intrusion', 'disguise', 'driving', 'explosive_devices', 'filch', 'hand_to_hand', 'infiltration', 'mechanics', 'medic', 'network', 'piloting', 'preparedness', 'sense_trouble', 'shooting', 'shrink', 'surveillance', 'weapons', 'accounting', 'archaeology', 'architecture', 'art_history', 'criminology', 'diagnosis', 'history', 'human_terrain', 'languages', 'law', 'military_science', 'occult_studies', 'research', 'vampirology', 'bullshit_detector', 'bureaucracy', 'cop_talk', 'flattery', 'flirting', 'high_society', 'interrogation', 'intimidation', 'negotiation', 'reassurance', 'streetwise', 'tradecraft', 'astronomy', 'chemistry', 'cryptography', 'data_recovery', 'electronic_surveillance', 'forensic_pathology', 'forgery', 'notice', 'outdoor_survival', 'pharmacy', 'photography', 'traffic_analysis', 'urban_survival', 'health', 'stability'];
+const armors = [
+  {
+    name: 'None',
+    resistance: {
+      bullets: '0',
+      melee: '0',
+      explosives: '0'
+    }
+  },
+  {
+    name: 'Ballistic cloth',
+    resistance: {
+      bullets: '1',
+      melee: '1',
+      explosives: '0'
+    }
+  },
+  {
+    name: 'Police Vest',
+    resistance: {
+      bullets: '2',
+      melee: '2',
+      explosives: '0'
+    }
+  },
+  {
+    name: 'Military Armor',
+    resistance: {
+      bullets: '3',
+      melee: '3',
+      explosives: '3'
+    }
+  },
+  {
+    name: 'Other',
+    resistance: {
+      bullets: '0',
+      melee: '0',
+      explosives: '0'
+    }
+  },
+];
+
+abilityValues.forEach((value) => {
+  abilities.forEach((ability) => {
+    on(`clicked:${ability}_pool_${value}`, (eventInfo) => {
+      const pool = `${ability}_pool`;
+      const payload = {};
+
+      getAttrs([pool, ability], function(values) {
+        if (values[pool] === '1' && value === '1') {
+          payload[pool] = 0;
+        } else if (parseInt(value, 10) > parseInt(values[ability], 10)) {
+          payload[pool] = values[pool];
+        } else {
+          payload[pool] = value;
+        }
+        setAttrs(payload);
+      });
+    });
+  }); 
+});
+
+abilities.forEach((ability) => {
+  on(`change:${ability}`, (eventInfo) => {
+    const pool = `${ability}_pool`;
+    const payload = {};
+    
+    payload[pool] = eventInfo.newValue;
+    setAttrs(payload);
+  });
+});
+
+on('change:armor_type', (eventInfo) => {
+  const armor = armors.find(el => el.name === eventInfo.newValue)
+
+  console.log(armor);
+
+  setAttrs({
+    armor_bullets: armor.resistance.bullets,
+    armor_melee: armor.resistance.melee,
+    armor_explosives: armor.resistance.explosives,
+  });
+});
+
+for (let value = 1; value <= 5; value++) {
+  on(`clicked:repeating_trust:trustlevelpool-${value}`, (eventInfo) => {
+    const pool = eventInfo.sourceAttribute.replace(`-${value}`, '');
+    const ability = eventInfo.sourceAttribute.replace(`pool-${value}`, '');
+    const payload = {};
+  
+    getAttrs([pool, ability], function(values) {
+      if (values[pool] === 1 && value === 1) {
+        payload[pool] = 0;
+      } else if (parseInt(value, 10) > parseInt(values[ability], 10)) {
+        payload[pool] = values[pool];
+      } else {
+        payload[pool] = value;
+      }
+      setAttrs(payload);
+    });
+  }); 
+}
